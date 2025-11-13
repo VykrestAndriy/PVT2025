196ac9129b9160d80c2173d50ac86281fe75df31
VykrestAndriy <andry20053004@gmail.com>
2025-11-13 18:47:24 +0200

    Add files via upload

---
+++ src/main/java/com/example/lab4/model/LaptopModel.java
@@ -0,0 +1,13 @@
+package com.example.lab4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LaptopModel {
+    private String name;
+    private String description;
+}
