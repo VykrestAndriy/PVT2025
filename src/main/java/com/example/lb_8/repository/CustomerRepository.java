cc22b8d6efc371e01e1776a1f358378aee240753
VykrestAndriy <andry20053004@gmail.com>
2025-11-20 12:42:35 +0200

    Delete src directory

--- src/main/java/com/example/lb_8/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lb_8.repository;
-
-import com.example.lb_8.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
